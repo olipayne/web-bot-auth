c5c4c5e9ad9dc4fc3c7a7375340e2242d6435ad6
Oliver Payne <opayne@cloudflare.com>
2025-06-06 21:35:05 +0200

    oops

---
+++ examples/browser-extension/.eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+}; 
